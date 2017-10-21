c71564d4c59ba4e7740ecd488a26b849fdca30c9
Drue Satterfield <drscp5@mst.edu>
2017-10-21 18:42:09 -0500

    removed .lib file from github's repo frontend.
    Binary file will now be coupled with source code via releases instead

--- Roveboard/tm4c1294ncpdt_API/RoveEthernet/RoveEthernet_TivaTM4C1294NCPDT.h
+++ Roveboard/tm4c1294ncpdt_API/RoveEthernet/RoveEthernet_TivaTM4C1294NCPDT.h
@@ -32,5 +32,5 @@ void roveEthernet_NetworkingStart(roveIP myIP);
 roveEthernet_Error roveEthernet_UdpSocketListen(uint16_t port);
 roveEthernet_Error roveEthernet_SendUdpPacket(roveIP destIP, uint16_t destPort, const uint8_t* msg, size_t msgSize);
 roveEthernet_Error roveEthernet_GetUdpMsg(roveIP* senderIP, void* buffer, size_t bufferSize);
-
+void roveEthernet_attachUdpReceiveCb(void (*userFunc)());
 #endif
