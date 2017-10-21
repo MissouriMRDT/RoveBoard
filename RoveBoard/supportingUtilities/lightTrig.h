9be4dd0ebde0f3d65af431f278e7979270a61b72
EliVerbrugge <eli.verbrugge@gmail.com>
2017-10-21 12:32:26 -0500

    Fixed implementation of trig functions

--- RoveBoard/supportingUtilities/lightTrig.h
+++ RoveBoard/supportingUtilities/lightTrig.h
@@ -2,10 +2,10 @@
 #define LIGHTTRIG_H
 
 
-
+//calculates taylor polynomial of sin of value
 float sinLW(int x);
 
-
+//calculates taylor polynomial of cos of value
 float cosLW(int x);
 
 #endif // LIGHTTRIG_H
